7734f8e6fa9aa9e6316628e8f93a1fea8d4f501f
Jerome <jerome.schmidt@students.makeschool.com>
2021-02-02 17:11:32 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+// Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.get('/', (req, res) => {
+  // res.send('Hello World!')
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
