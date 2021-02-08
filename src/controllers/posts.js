424fadf016c0071c4f3d3e8099a361e5560d17fb
Jerome <jerome.schmidt@students.makeschool.com>
2021-02-08 15:19:11 -0600

    Users can see individual posts

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -14,4 +14,15 @@ module.exports = (app) => {
     })
   });
 
+  app.get("/posts/:id", function(req, res) {
+  // LOOK UP THE POST
+  Post.findById(req.params.id).lean()
+    .then(post => {
+      res.render("layouts/posts-show", { post });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+    });
+
 };
