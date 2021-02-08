35aca09f21bd5fb333d99e1628d2dc9439ad822b
Jerome <jerome.schmidt@students.makeschool.com>
2021-02-08 15:41:50 -0600

    Users can see Subreddits

---
+++ controllers/posts.js
@@ -0,0 +1,39 @@
+const Post = require('../models/post');
+
+module.exports = (app) => {
+
+  // CREATE
+  app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body);
+
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save((err, post) => {
+      // REDIRECT TO THE ROOT
+      return res.redirect(`/`);
+    })
+  });
+
+  app.get("/posts/:id", function(req, res) {
+  // LOOK UP THE POST
+  Post.findById(req.params.id).lean()
+    .then(post => {
+      res.render("layouts/posts-show", { post });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+    });
+
+    // SUBREDDIT
+    app.get("/n/:subreddit", function(req, res) {
+    Post.find({ subreddit: req.params.subreddit }).lean()
+      .then(posts => {
+        res.render("layouts/posts-index", { posts });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    });
+
+};
--- server.js
+++ server.js
@@ -1,20 +1,54 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-
-// Middleware
+// require('dotenv/config');
+const port=3000
+const path = require("path");
+const express = require('express');
 const exphbs  = require('express-handlebars');
+const app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+app.engine('handlebars', exphbs({
+    layoutsDir: __dirname + '/views/layouts',
+    defaultLayout: 'main'
+}));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
+require('./controllers/posts.js')(app);
+require('./controllers/posts.js')(app);
+// Set db
+require('./data/reddit-db');
+
+const Post = require('./models/post');
 
 app.get('/', (req, res) => {
-  // res.send('Hello World!')
-  res.render('home')
+  Post.find({}).lean()
+    .then(posts => {
+      res.render('layouts/posts-index', { posts });
+    })
+    .catch(err => {
+      console.log(err.message);
+    })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get("/posts/new", function(req,res){
+    return res.render("layouts/posts-new")
 })
+
+
+// Start Server
+app.listen(3000, () => {
+    console.log(`Example app listening on port ${port}!`)
+})
+
+module.exports = app;
